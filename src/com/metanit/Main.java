a8accf95d86a4528fb9055f1bb15e40d254a8038
StasKrylov <STSSTD2021@gmail.com>
2021-03-23 03:18:59 +0300

    lesson1

---
+++ src/com/metanit/Main.java
@@ -0,0 +1,12 @@
+package com.metanit;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Garry say \n     \" Я выучу английский за месяц \" ");
+        int a = 5;
+        System.out.println(a++); // 5
+        int b = 5;
+        System.out.println(++b); // 6
+    }
+}
---
+++ src/lesson2/L2ex1.java
@@ -0,0 +1,17 @@
+package lesson2;
+
+public class L2ex1 {
+    public static void main(String[] args) {
+        int a = 0;
+        if (a<0) {
+            System.out.println(" Это отрицательное число ");
+        } if (a>0){
+            System.out.println( " Это положительное число ");
+        }
+        else {
+            System.out.println( " Число равно нулю ");
+        }
+
+
+    }
+}
